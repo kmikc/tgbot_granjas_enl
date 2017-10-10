2c43918a817aa821e90bed4c1ce539def3c0e317
jParkes <rodrigo.parkes@gmail.com>
2017-10-10 01:37:48 -0300

    Agregado conversationhandler, hay que ver lo del received_informaion y guardar

--- granjasenl_bot.py
+++ granjasenl_bot.py
@@ -2,12 +2,28 @@
 # -*- coding: utf-8 -*-
 
 #from peewee import *
-from telegram.ext import Updater, CommandHandler, Job, ConversationHandler
+from telegram.ext import Updater, CommandHandler, Job, ConversationHandler, MessageHandler, Filters
 import logging
 
 from models import Granja, Participantes
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+FECHA, LUGAR, COMENTARIO = range(3)
+
+
+def received_information(bot, update, user_data):
+    text = update.message.text
+    category = user_data['choice']
+    user_data[category] = text
+    del user_data['choice']
+
+    update.message.reply_text("Neat! Just so you know, this is what you already told me:"
+                              "%s"
+                              "You can tell me more, or change your opinion on something."
+                              % facts_to_str(user_data),
+                              reply_markup=markup)
+    return FECHA
 
 #
 #
@@ -32,13 +48,38 @@ def granja(bot, update):
     update.message.reply_text(msg_str)
 
 
+def cancel(bot, update):
+    print "Cancel"
+    update.message.reply_text('Cancel')
+    return ConversationHandler.END
+
+#
+#
+# ConversationHandler
+#
+#
+
+conv_handler = ConversationHandler(
+    entry_points = [CommandHandler('granja', granja)],
+
+    states = {
+        FECHA: [MessageHandler(Filters.text, received_information, pass_user_data=True)],
+        LUGAR: [MessageHandler(Filters.text, received_information, pass_user_data=True)],
+        COMENTARIO: [MessageHandler(Filters.text, received_information, pass_user_data=True)]
+    },
+
+    fallbacks = [CommandHandler('cancel', cancel)]
+)
+
 # TOKEN
 token = open('TOKEN').read().rstrip('\n')
 updater = Updater(token)
 
 # COMANDOS
 updater.dispatcher.add_handler(CommandHandler('info', info))
-updater.dispatcher.add_handler(CommandHandler('granja', granja))
+#updater.dispatcher.add_handler(CommandHandler('granja', granja))
+
+updater.dispatcher.add_handler(conv_handler)
 
 # JOB QUEUE
 #jobqueue = updater.job_queue
